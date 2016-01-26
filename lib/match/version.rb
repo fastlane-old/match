5b0df073b6dd949f74ab7a22cf473ebe3eee9a70
Jakob Jensen <jje@trifork.com>
2016-01-26 23:45:52 +0100

    Corrected rubocop offences.

--- lib/match/version.rb
+++ lib/match/version.rb
@@ -1,4 +1,4 @@
 module Match
-  VERSION = "0.2.5"
-  DESCRIPTION = "Easily sync your certificates and profiles across your team using git"
+  VERSION = "0.2.5".freeze
+  DESCRIPTION = "Easily sync your certificates and profiles across your team using git".freeze
 end
